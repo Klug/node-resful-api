99b0a4286cfab1994a412c5c1c67ca19eacbbfc6
PhilippKlug <pk2604DkE3>
2018-10-22 16:30:43 +0200

    Added MongoDB+Mongoose. Config routes/products.js for DB access

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 //URL:https://academind.com/learn/node-js/building-a-restful-api-with/
-//Last video: Parsing the Body & Handling CORS
+//Last video: MongoDB and Mongoose
 
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+//mongodb://node-rest-shop-admin:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-suk4z.mongodb.net:27017,node-rest-shop-shard-00-01-suk4z.mongodb.net:27017,node-rest-shop-shard-00-02-suk4z.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true
+mongoose.connect('mongodb://node-rest-shop-admin:Sdfghk!123@node-rest-shop-shard-00-00-suk4z.mongodb.net:27017,node-rest-shop-shard-00-01-suk4z.mongodb.net:27017,node-rest-shop-shard-00-02-suk4z.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true',
+    {
+        // useMongoClient: true
+        useNewUrlParser: true
+    })
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
